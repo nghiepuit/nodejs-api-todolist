106885e84514d077b9283ffb08f6b18869654f3a
nghiepphan <nghiep.phan@saigontechnology.com>
2019-04-28 16:21:23 +0700

    add

---
+++ config/database.js
@@ -0,0 +1,16 @@
+module.exports = {
+  // development: {
+  //   dialect: "sqlite",
+  //   storage: "./db.development.sqlite"
+  // },
+  production: {
+    url: process.env.DATABASE_URL,
+    dialect: "mysql",
+    define: {
+      underscored: false
+    },
+    username: "root",
+    password: "Nghiep2014",
+    database: "db_nodejs"
+  }
+};

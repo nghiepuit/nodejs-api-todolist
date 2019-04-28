6de1aebef3b843a7a1f73a6e287ced94cc389666
nghiepphan <nghiep.phan@saigontechnology.com>
2019-04-28 16:26:34 +0700

    add

--- src/infra/database/models/role.js
+++ src/infra/database/models/role.js
@@ -26,16 +26,17 @@ module.exports = (sequelize, DataTypes) => {
         associate: function(models) {
           // associations can be defined here
           // based on file name: user.js => model.user
-          Role.belongsToMany(models.user, {
-            through: "userRoles",
-            foreignKey: "roleId",
-            as: "users"
-          });
-          Role.belongsToMany(models.permission, {
-            through: "rolePermissions",
-            foreignKey: "roleId",
-            as: "permissions"
-          });
+          console.log("models: ", models);
+          // Role.belongsToMany(models.user, {
+          //   through: "userRoles",
+          //   foreignKey: "roleId",
+          //   as: "users"
+          // });
+          // Role.belongsToMany(models.permission, {
+          //   through: "rolePermissions",
+          //   foreignKey: "roleId",
+          //   as: "permissions"
+          // });
         }
       }
     }

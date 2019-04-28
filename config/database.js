58fab5a83afbbf293eb3a0af9b9a8b7dcf4eac76
nghiepphan <nghiep.phan@saigontechnology.com>
2019-04-28 17:58:02 +0700

    update charset

--- config/database.js
+++ config/database.js
@@ -1,13 +1,24 @@
 module.exports = {
   development: {
     dialect: "sqlite",
-    storage: "./db.development.sqlite"
+    storage: "./db.development.sqlite",
+    define: {
+      charset: "utf8mb4",
+      freezeTableName: true,
+      underscored: true,
+      underscoredAll: true,
+      timestamps: false
+    }
   },
   production: {
     url: process.env.DATABASE_URL,
     dialect: "mysql",
     define: {
-      underscored: false
+      charset: "utf8mb4",
+      freezeTableName: true,
+      underscored: true,
+      underscoredAll: true,
+      timestamps: false
     },
     username: "root",
     password: "Nghiep2014",

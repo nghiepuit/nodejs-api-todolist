c921bea7bdbd384273530581cf9b87e020da9bd9
nghiepuit <nghiepuit@gmail.com>
2019-07-06 14:57:34 +0700

    [master] add utf-8

--- src/infra/database/migrate/20190627115947-create-task.js
+++ src/infra/database/migrate/20190627115947-create-task.js
@@ -1,39 +1,46 @@
 "use strict";
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable("tasks", {
-      id: {
-        type: Sequelize.UUID,
-        defaultValue: Sequelize.UUIDV4,
-        primaryKey: true,
-        allowNull: false
+    return queryInterface.createTable(
+      "tasks",
+      {
+        id: {
+          type: Sequelize.UUID,
+          defaultValue: Sequelize.UUIDV4,
+          primaryKey: true,
+          allowNull: false
+        },
+        title: {
+          type: Sequelize.STRING,
+          unique: true
+        },
+        description: {
+          type: Sequelize.STRING
+        },
+        status: {
+          type: Sequelize.INTEGER,
+          defaultValue: 0,
+          allowNull: false
+        },
+        order: {
+          type: Sequelize.INTEGER
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.NOW
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.NOW
+        }
       },
-      title: {
-        type: Sequelize.STRING,
-        unique: true
-      },
-      description: {
-        type: Sequelize.STRING
-      },
-      status: {
-        type: Sequelize.INTEGER,
-        defaultValue: 0,
-        allowNull: false
-      },
-      order: {
-        type: Sequelize.INTEGER
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.NOW
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.NOW
+      {
+        charset: "utf8mb4",
+        collate: "utf8mb4_unicode_ci"
       }
-    });
+    );
   },
   down: (queryInterface, Sequelize) => {
     return queryInterface.dropTable("tasks");
